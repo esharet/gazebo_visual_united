0ad689ad140bc4310271b40410757b80e408b2aa
zb <zb@int.com>
2021-10-18 13:48:26 +0300

    messages fixes

--- gazebo_unity_messages/__init__.py
+++ gazebo_unity_messages/__init__.py
@@ -2,9 +2,10 @@
 # in root project dir- 
 # ```protoc -I=proto --python_out=msgs proto/*```
 
-from py_msgs.ack_pb2 import Ack
-from py_msgs.camera_config_pb2 import CameraConfig
-from py_msgs.camera_position_pb2 import CameraPosition
+from gazebo_unity_messages.py_msgs.ack_pb2 import Ack
+from gazebo_unity_messages.py_msgs.camera_config_pb2 import CameraConfig
+from gazebo_unity_messages.py_msgs.camera_position_pb2 import CameraPosition
+
 
 
 
